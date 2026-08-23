dc41c2af413a85b5d48672c3fb99b00433379498
sakkol-git <chensakkol1124@gmail.com>
2026-08-23 09:04:45 +0700

    change: input new project data 3

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "*.supabase.co",
+      },
+      {
+        protocol: "https",
         hostname: "**",
       },
     ],
